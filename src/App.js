5bef144b2f71cf4cfea66fc22661a45e21ff03d6
Eswar Benarjee <eswar.bane2002.india@gmail.com>
2025-02-18 02:34:09 +0530

    Updating Homepage

--- src/App.js
+++ src/App.js
@@ -13,8 +13,8 @@ function App() {
         <Route
           path="/"
           element={
-              <Home />
             <>
+              <Home />
               <EmojiRain />
             </>
           }
